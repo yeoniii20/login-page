b1f881938f369bcbaa2736375ad289c8bee3d03a
KwonSuyeon <commetoi_yeoni@naver.com>
2023-02-18 03:14:39 +0900

    블필요한 스타일 코드 삭제

--- src/constant/loginPage.styled.js
+++ src/constant/loginPage.styled.js
@@ -1,21 +1,9 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-  background-color: pink;
-`;
-
-export const Container = styled.div`
-  background-color: pink;
-`;
-
-export const LoginForm = styled.input`
-  background-color: pink;
-`;
+// import styled from "styled-components";
 
 // export const Wrapper = styled.div`
 //   background-color: pink;
 // `;
 
-// export const Wrapper = styled.div`
+// export const Container = styled.div`
 //   background-color: pink;
 // `;
